456a4d823c0d9624e76aeeb6b7eac8edc0163810
Nasir <nasirkhimani@gmail.com>
2023-08-09 12:54:46 -0400

    inital commit -> new website in progress

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Header from '../partials/Header';
+import PageIllustration from '../partials/PageIllustration';
+import HeroHome from '../partials/HeroHome';
+import FeaturesZigZag from '../partials/FeaturesZigzag';
+import Banner from '../partials/Banner';
+import Footer from '../partials/Footer';
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="grow">
+        {/*  Page illustration */}
+        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+          <PageIllustration />
+        </div>
+
+        {/*  Page sections */}
+        <HeroHome />
+        <FeaturesZigZag />
+      </main>
+
+      <Banner />
+
+      {/*  Site footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../partials/Header';
+import PageIllustration from '../partials/PageIllustration';
+import Banner from '../partials/Banner';
+
+function SignIn() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="grow">
+
+        {/*  Page illustration */}
+        <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+          <PageIllustration />
+        </div>
+
+        <section className="relative">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+              {/* Page header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h1 className="h1">Welcome back!</h1>
+              </div>
+
+              {/* Form */}
+              <div className="max-w-sm mx-auto">
+                <form>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="email">Email</label>
+                      <input id="email" type="email" className="form-input w-full text-gray-300" placeholder="you@youremail.com" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">Password</label>
+                      <input id="password" type="password" className="form-input w-full text-gray-300" placeholder="Password (at least 10 characters)" required />
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <div className="flex justify-between">
+                        <label className="flex items-center">
+                          <input type="checkbox" className="form-checkbox" />
+                          <span className="text-gray-400 ml-2">Keep me signed in</span>
+                        </label>
+                        <Link to="/reset-password" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">Forgot Password?</Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button className="btn text-white bg-purple-600 hover:bg-purple-700 w-full">Sign in</button>
+                    </div>
+                  </div>
+                </form>
+                <div className="text-gray-400 text-center mt-6">
+                  Don’t you have an account? <Link to="/signup" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">Sign up</Link>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+
+      </main>
+
+      <Banner />
+
+    </div>
+  );
+}
+
+export default SignIn;
