3d90d11e7f9e0591d7f8b971c50d956187511acb
nasir <nasirkhimani@gmail.com>
2023-09-01 22:41:29 -0400

    --chore update and dono update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
+import Donation from './pages/Donation';
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path= "/donation" element={<Donation />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
---
+++ src/partials/DonoCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const DonoCard = ({name, address, qrcode}) => {
+  return (
+    <div data-aos="fade-up" data-aos-delay="400">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-800">
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                    <p className="text-gray-400 text-base break-words">
+                        {address}
+                    </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded px-3 py-3 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <img src={qrcode}/>
+                </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DonoCard
