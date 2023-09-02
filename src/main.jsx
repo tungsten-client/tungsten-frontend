41754506ae678e1c2754dccc49cf78fafcb02e79
nasir <nasirkhimani@gmail.com>
2023-09-01 23:58:55 -0400

    WHAT THE FUCK

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename = "/tungesten-frontend">
+    <BrowserRouter>
       <App />
     </BrowserRouter >
 
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     postcss,
   },
   plugins: [react()],
-  base: '/tungesten-frontend/',
+  base: '/',
   resolve: {
     alias: [
       {
