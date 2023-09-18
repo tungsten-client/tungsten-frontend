7a61577dc9ba689383855d37aca5111bc53550c5
xrexy <rexydoesthings@gmail.com>
2023-09-18 21:50:15 +0300

    Upgrade layout & DX fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import "aos/dist/aos.css";
+import "./css/style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter
+} from "react-router-dom";
+
+import Donation from "./pages/Donation";
+import Home from "./pages/Home";
+import ResetPassword from "./pages/ResetPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "donation",
+        Component: Donation,
+      },
+      {
+        path: "signin",
+        Component: SignIn,
+      },
+      {
+        path: "signup",
+        Component: SignUp,
+      },
+      {
+        path: "reset-password",
+        Component: ResetPassword,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter >
-
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,28 +1,9 @@
-import React from 'react'
+import React from "react";
 
-import Banner from '../partials/Banner';
-import PageIllustration from '../partials/PageIllustration';
-import Header from '../partials/Header';
-import DonationBox from '../partials/DonationBox';
+import DonationBox from "../partials/DonationBox";
 
 const Donation = () => {
-  return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      {/*  Site header */}
-      <Header />
+  return <DonationBox />;
+};
 
-      {/*  Page content */}
-      <main className="grow">
-        <div className="relative h-0 max-w-6xl mx-auto pointer-events-none" aria-hidden="true">
-          <PageIllustration />
-        </div>
-        <DonationBox/>
-      </main>
-      <Banner/>
-
-      {/*  Site footer */}
-    </div>
-  )
-}
-
-export default Donation
+export default Donation;
